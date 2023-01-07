e5559ae05a74fe1f8e2e3ee302048faebfa3b72d
Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>
2023-01-07 15:17:00 +0530

    create drop down for signup page

---
+++ Client/bloomevents/src/components/SignupDropDown.tsx
@@ -0,0 +1,29 @@
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+function SignupDropDown({val,array, func, title}:any) {
+    return (
+        <FormControl variant="standard" fullWidth sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-standard-label">{title}</InputLabel>
+            <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={val}
+            onChange={(e) => func(e.target.value)}
+            label={title}
+            >
+            <MenuItem value="" disabled>
+                <em>None</em>
+            </MenuItem>
+    
+            {array.map((e:any,i:number)=>(
+                <MenuItem value={e.id.toString()} key={i+1}>{e.name}</MenuItem>
+            ))}
+            </Select>
+        </FormControl>
+      )
+}
+
+export default SignupDropDown
