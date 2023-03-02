900bc741272469e9cdfd77668629a9355fefd162
Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>
2023-03-02 13:23:20 +0530

    bind packages of event page

--- Client/bloomevents/src/components/Cards/EventDetailCard.tsx
+++ Client/bloomevents/src/components/Cards/EventDetailCard.tsx
@@ -1,30 +1,87 @@
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { RouteName } from "constant/routeName";
+import PackageServices from "Services/Packages/PackageService";
+import { toast } from "react-toastify";
+import ProviderService from "Services/Provider/ProviderServices";
 
-function EventDetailCard({ providername, packagename }: any) {
+function EventDetailCard({ packageId, placed, func, addToEventId }: any) {
+  //console.log(placed);
   const handleDeleteEvent = () => {
-    alert(`${providername} - ${packagename} deleted.`);
+    func(addToEventId);
+    alert(`${packageId} deleted.`);
   };
+
+  const [packge, setPackge] = React.useState<any>();
+
+  React.useEffect(() => {
+    PackageServices.getPackageByPackageId(packageId).then((res: any) => {
+      if (res.data.status == 1) {
+        setPackge(res.data.data);
+        //console.log(res.data.data);
+        return;
+      } else {
+        toast.error(res.data.message);
+      }
+    });
+  }, []);
+
+  const [provider, setProvider] = React.useState<any>();
+  React.useEffect(() => {
+    if (packge) {
+      ProviderService.getProvider(packge.packageId).then((res: any) => {
+        if (res.data.status == 1) {
+          setProvider(res.data.data);
+          //console.log(res.data.data);
+          return;
+        } else {
+          toast.error(res.data.message);
+        }
+      });
+    }
+  }, [packge]);
+
   return (
     <div>
-      <div className="card w-96 glass">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="car!" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title text-[#ffa537]">{providername}</h2>
-          <p className="text-[#000]">{packagename}</p>
-          <div className="justify-end mt-3 card-actions">
-            <Button
-              variant="outlined"
-              color="error"
-              onClick={handleDeleteEvent}>
-              <DeleteIcon /> Delete
-            </Button>
-          </div>
-        </div>
-      </div>
+      {packge && provider && (
+        <>
+          <Link
+            to={{
+              pathname: `${RouteName.ProviderDetails.replace(
+                ":providerId",
+                packge.providerId.toString()
+              )}`,
+            }}
+            className="w-10/12">
+            <div className="card w-96 glass">
+              <figure>
+                <img src="https://placeimg.com/400/225/arch" alt="car!" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-[#ffa537]">
+                  {provider.businessName}
+                </h2>
+                <p className="text-[#000]">{packge.packageName}</p>
+              </div>
+            </div>
+          </Link>
+
+          {placed == false && (
+            <div className="justify-end mt-3 card-actions">
+              <button
+                type="button"
+                onClick={handleDeleteEvent}
+                className="text-red-600 border-red-600 hover:bg-red-600 my-event-card-btn">
+                <span className="mr-1">
+                  <DeleteIcon />
+                </span>
+                Remove Package
+              </button>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
