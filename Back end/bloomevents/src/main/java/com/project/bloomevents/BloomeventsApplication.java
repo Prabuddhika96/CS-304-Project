b5b5231a602df636a841dbf32cb293d43447d696
Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>
2023-03-11 14:45:09 +0530

    update application properties

--- Back end/bloomevents/src/main/java/com/project/bloomevents/BloomeventsApplication.java
+++ Back end/bloomevents/src/main/java/com/project/bloomevents/BloomeventsApplication.java
@@ -1,11 +1,16 @@
 package com.project.bloomevents;
 
+import com.project.bloomevents.FileUpload.FileStorageProperties;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class BloomeventsApplication {
 
 	public static void main(String[] args) {
